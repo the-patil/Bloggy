56e87c12e605258554f97e3f93e37b9c3577ebf8
Vaibhav <vaibhavmhaske.here@gmail.com>
2024-04-25 02:10:36 +0530

    some changes

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,85 +1,85 @@
+import { signinInput, signupInput } from "@100xdevs/medium-common";
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 import { Hono } from "hono";
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign } from 'hono/jwt'
-import { signupInput, signinInput } from "@100xdevs/medium-common";
+import { sign } from 'hono/jwt';
 
 export const userRouter = new Hono<{
-    Bindings: {
-        DATABASE_URL: string;
-        JWT_SECRET: string;
-    }
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  }
 }>();
 
 userRouter.post('/signup', async (c) => {
-    const body = await c.req.json();
-    const { success } = signupInput.safeParse(body);
-    if (!success) {
-        c.status(411);
-        return c.json({
-            message: "Inputs not correct"
-        })
-    }
-    const prisma = new PrismaClient({
-      datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate())
-  
-    try {
-      const user = await prisma.user.create({
-        data: {
-          username: body.username,
-          password: body.password,
-          name: body.name
-        }
-      })
-      const jwt = await sign({
-        id: user.id
-      }, c.env.JWT_SECRET);
-  
-      return c.text(jwt)
-    } catch(e) {
-      console.log(e);
-      c.status(411);
-      return c.text('Invalid')
-    }
-  })
-  
-  
-  userRouter.post('/signin', async (c) => {
-    const body = await c.req.json();
-    const { success } = signinInput.safeParse(body);
-    if (!success) {
-        c.status(411);
-        return c.json({
-            message: "Inputs not correct"
-        })
-    }
+  const body = await c.req.json();
+  const { success } = signupInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({
+      message: "Inputs not correct"
+    })
+  }
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
 
-    const prisma = new PrismaClient({
-      datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate())
-  
-    try {
-      const user = await prisma.user.findFirst({
-        where: {
-          username: body.username,
-          password: body.password,
-        }
-      })
-      if (!user) {
-        c.status(403);
-        return c.json({
-          message: "Incorrect creds"
-        })
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: body.username,
+        password: body.password,
+        name: body.name
       }
-      const jwt = await sign({
-        id: user.id
-      }, c.env.JWT_SECRET);
-  
-      return c.text(jwt)
-    } catch(e) {
-      console.log(e);
-      c.status(411);
-      return c.text('Invalid')
+    })
+    const jwt = await sign({
+      id: user.id
+    }, c.env.JWT_SECRET);
+
+    return c.text(jwt)
+  } catch (e) {
+    console.log(e);
+    c.status(411);
+    return c.text('Invalid')
+  }
+})
+
+
+userRouter.post('/signin', async (c) => {
+  const body = await c.req.json();
+  const { success } = signinInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({
+      message: "Inputs not correct"
+    })
+  }
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        username: body.username,
+        password: body.password,
+      }
+    })
+    if (!user) {
+      c.status(403);
+      return c.json({
+        message: "Incorrect creds"
+      })
     }
-  })
+    const jwt = await sign({
+      id: user.id
+    }, c.env.JWT_SECRET);
+
+    return c.text(jwt)
+  } catch (e) {
+    console.log(e);
+    c.status(411);
+    return c.text('Invalid')
+  }
+})
