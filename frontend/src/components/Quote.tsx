802fb1c1357d7bbabbf51087619705417717ed81
Vaibhav <vaibhavmhaske.here@gmail.com>
2024-05-02 01:26:34 +0530

    fe changes

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,4 +1,3 @@
-
 export const Quote = () => {
     return <div className="bg-slate-200 h-screen flex justify-center flex-col">
         <div className="flex justify-center">
@@ -14,6 +13,6 @@ export const Quote = () => {
                 </div>
             </div>
         </div>
-        
+
     </div>
 }
