789d255b1cf30440a10b653a71ee22568bbc4cad
Vaibhav <vaibhavmhaske.here@gmail.com>
2024-05-05 01:52:15 +0530

    fe changes

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,28 +1,74 @@
-import { ChangeEvent } from "react";
-import { Link } from "react-router-dom";
+import { SignupInput } from "@code1/medium-common";
+import axios from "axios";
+import { ChangeEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BACKEND_URL } from "../config";
 
 export const Auth = ({ type }: { type: "signup" | "signin" }) => {
+    const navigate = useNavigate();
+    const [postInputs, setPostInputs] = useState<SignupInput>({
+        name: "",
+        username: "",
+        password: ""
+    });
+
+    async function sendRequest() {
+        try {
+            const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`, postInputs);
+            const jwt = response.data;
+            localStorage.setItem("token", jwt);
+            navigate("/blogs");
+        } catch (e) {
+            alert("Error while signing up")
+            // alert the user here that the request failed
+        }
+    }
+
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
+            <div>
+                <div className="px-10">
+                    <div className="text-3xl font-extrabold">
+                        Create an account
+                    </div>
+                    <div className="text-slate-500">
+                        {type === "signin" ? "Don't have an account?" : "Already have an account?"}
+                        <Link className="pl-2 underline" to={type === "signin" ? "/signup" : "/signin"}>
+                            {type === "signin" ? "Sign up" : "Sign in"}
+                        </Link>
+                    </div>
+                </div>
+                <div className="pt-8">
+                    {type === "signup" ? <LabelledInput label="Name" placeholder="Harkirat Singh..." onChange={(e) => {
+                        setPostInputs({
+                            ...postInputs,
+                            name: e.target.value
+                        })
+                    }} /> : null}
+                    <LabelledInput label="Username" placeholder="harkirat@gmail.com" onChange={(e) => {
+                        setPostInputs({
+                            ...postInputs,
+                            username: e.target.value
+                        })
+                    }} />
+                    <LabelledInput label="Password" type={"password"} placeholder="123456" onChange={(e) => {
+                        setPostInputs({
+                            ...postInputs,
+                            password: e.target.value
+                        })
+                    }} />
+                    <button onClick={sendRequest} type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
+                </div>
+            </div>
         </div>
-        <div className="text-xl font-extrabold">
-            create an account
-        </div>
-        <div className="text-slate-400">
-            <Link to={"/signin"} className="pl-2 underline" >Already have an account?</Link>
-        </div>
-        <LabelledInput label="Username" placeholder="JonDoe@gmail.com" onChange={(e) => {
-
-        }}></LabelledInput>
     </div>
 }
 
-console.log("enter you number")
 interface LabelledInputType {
     label: string;
     placeholder: string;
     onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-    type?: string
+    type?: string;
 }
 
 function LabelledInput({ label, placeholder, onChange, type }: LabelledInputType) {
